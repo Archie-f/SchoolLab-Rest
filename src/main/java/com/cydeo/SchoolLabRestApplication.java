7d1feec3a2ef9a253e450853a05b2c3999f463e7
Archie-f <akif.mk66@gmail.com>
2024-01-04 21:17:13 +0300

    master: SchoolLab-Rest started.

---
+++ src/main/java/com/cydeo/SchoolLabRestApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolLabRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolLabRestApplication.class, args);
+    }
+
+}
